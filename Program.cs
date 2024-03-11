58c3d42602fe497fba6e8efebb408fe98e06b2e2
veerharis <130150350+veerharis@users.noreply.github.com>
2024-03-11 17:17:35 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Entre Your temperature \n 1.Celsius \n 2. Fahrenheit");
+
+
+int Celsius = Convert.ToInt32(Console.ReadLine());
+
+if (Celsius == 1)
+{
+    Console.WriteLine("Entre Your  temperature  Celsius");
+}
+
+Console.ReadKey();
+
+
